562a35ffe87b5d3d77d131c0575b2d0cb9bbc462
Dmitry Nikitenko <dima.nikitenko@gmail.com>
2021-05-08 16:26:37 +0600

    read feature flags from env

--- club/features.py
+++ club/features.py
@@ -1,3 +1,5 @@
-PUBLIC_CONTENT = True
-EMAIL_SIGN_UP = True
-FREE_MEMBERSHIP = True
+import os
+
+PUBLIC_CONTENT = os.getenv("PUBLIC_CONTENT") or False
+EMAIL_SIGN_UP = os.getenv("EMAIL_SIGN_UP") or False
+FREE_MEMBERSHIP = os.getenv("FREE_MEMBERSHIP") or False
