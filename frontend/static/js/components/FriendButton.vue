4a32a524b41263149f46c09dc9c1871e0535e22f
Dmitry Nikitenko <dima.nikitenko@gmail.com>
2021-06-19 14:08:31 +0600

    rename dudes to followers

--- frontend/static/js/components/FriendButton.vue
+++ frontend/static/js/components/FriendButton.vue
@@ -1,8 +1,8 @@
 <template>
     <span>
         <button class="button friend-button" @click="toggle">
-            <span v-if="isFriend" class="friend-button-text">✅ Мой чувак</span>
-            <span v-else class="friend-button-text">🤝 Добавить в мои чуваки</span>
+            <span v-if="isFriend" class="friend-button-text">✅ Вы подписаны</span>
+            <span v-else class="friend-button-text">🤝 Подписаться</span>
         </button>
     </span>
 </template>
