c6f527f996e111a27f0cf83b40b2dc38b0027389
Dmitry Nikitenko <dima.nikitenko@gmail.com>
2021-06-28 13:06:10 +0600

    https urls in sitemap

--- posts/sitemaps.py
+++ posts/sitemaps.py
@@ -4,6 +4,8 @@ from posts.models.post import Post
 
 
 class PublicPostsSitemap(Sitemap):
+    protocol = "https"
+
     def items(self):
         return Post.visible_objects().filter(is_public=True)
 
