1df96a0df223e31d127a06e0a30629b89c4d548c
Dmitry Nikitenko <dima.nikitenko@gmail.com>
2021-10-06 10:26:55 +0600

    fix feature flags

--- club/features.py
+++ club/features.py
@@ -1,5 +1,5 @@
 import os
 
-PUBLIC_CONTENT = os.getenv("PUBLIC_CONTENT") or False
-EMAIL_SIGN_UP = os.getenv("EMAIL_SIGN_UP") or False
-FREE_MEMBERSHIP = os.getenv("FREE_MEMBERSHIP") or False
+PUBLIC_CONTENT = os.getenv("PUBLIC_CONTENT") == 'true' or False
+EMAIL_SIGN_UP = os.getenv("EMAIL_SIGN_UP") == 'true' or False
+FREE_MEMBERSHIP = os.getenv("FREE_MEMBERSHIP") == 'true' or False
