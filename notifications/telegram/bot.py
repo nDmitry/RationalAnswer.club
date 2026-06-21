446e307a8d45b7395e4649cbe46687b1b50f832e
Dmitry Nikitenko <dima.nikitenko@gmail.com>
2026-06-21 11:35:40 +0600

    Fix proxy usage in telegram.Bot class instantiation

--- notifications/telegram/bot.py
+++ notifications/telegram/bot.py
@@ -2,14 +2,15 @@ import logging
 
 import telegram
 from django.conf import settings
+from telegram.utils.request import Request
 
 log = logging.getLogger()
 
-_request_kwargs = None
+_request = None
 if settings.TELEGRAM_PROXY_URL:
-    _request_kwargs = {"proxy_url": settings.TELEGRAM_PROXY_URL}
+    _request = Request(proxy_url=settings.TELEGRAM_PROXY_URL)
 bot = (
-    telegram.Bot(token=settings.TELEGRAM_TOKEN, request_kwargs=_request_kwargs)
+    telegram.Bot(token=settings.TELEGRAM_TOKEN, request=_request)
     if settings.TELEGRAM_TOKEN
     else None
 )
